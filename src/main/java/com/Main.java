16d3652a5ac7dbf4a3c05fd7e38804ebdd4b2e18
montgomeri15 <montgomeri15@gmail.com>
2017-09-15 08:36:39 +0300

    10

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -6,18 +6,12 @@ import java.util.Date;
 
 public class Main {
 
-    private static String LOG_PROPERTIES_FILE = "./src/main/resources/log4j.properties";
     private static Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException {
 
-        Config config = new Config(LOG_PROPERTIES_FILE);
-        config.init();
-
         logger.info("Starting the program...");
         logger.info("start in the next line...", new Exception());
-        Date date = new Date();
-        GetTime getTime = new GetTime();
-        getTime.Time(date);
+        GetTime.Time(new Date());
     }
 }
